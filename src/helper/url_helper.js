9b7efdb382b8ee418ec0bb39e9365470ba8f4be0
SanketJadhav03 <jadhavsanket1303@gmail.com>
2025-11-18 16:09:45 +0530

    url helper

--- src/helper/url_helper.js
+++ src/helper/url_helper.js
@@ -1,4 +1,4 @@
-// export const API_URL = "https://chhatrapati-api.vercel.app/api";
-// export const IMG_API_URL = "https://chhatrapati-api.vercel.app";
-export const API_URL = "http://localhost:8080/api";
-export const    IMG_API_URL = "http://localhost:8080";
+export const API_URL = "https://chhatrapati-api.vercel.app/api";
+export const IMG_API_URL = "https://chhatrapati-api.vercel.app";
+// export const API_URL = "http://localhost:8080/api";
+// export const    IMG_API_URL = "http://localhost:8080";
